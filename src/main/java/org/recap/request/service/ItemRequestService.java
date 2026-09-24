4f047d296144142c178ecbc6942699109c957eaf
NSSuresh11 <nssuresh.subramanian@gmail.com>
2026-09-24 10:24:52 +0530

    Request Status API Code changes

--- src/main/java/org/recap/request/service/ItemRequestService.java
+++ src/main/java/org/recap/request/service/ItemRequestService.java
@@ -1455,7 +1455,7 @@ public class ItemRequestService {
                 requestStatusResponse.setRequestingInstitution(requestItemEntity.getInstitutionEntity().getInstitutionCode());
                 requestStatusResponse.setRequestType(requestItemEntity.getRequestTypeEntity().getRequestTypeCode());
                 requestStatusResponse.setDeliveryLocation(requestItemEntity.getStopCode());
-                requestStatusResponse.setRequestNotes(requestItemEntity.getNotes());
+                requestStatusResponse.setRequestNotes(requestItemEntity.getNotes().replace("\\n", System.lineSeparator()));
                 requestStatusResponse.setSuccess(Boolean.TRUE);
                 requestStatusResponses.add(requestStatusResponse);
             }
